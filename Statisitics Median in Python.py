460cedd71502558164a6be71936f5021da223708
Ali Hassan <alihasanjatoi@gmail.com>
2021-05-16 01:17:47 +0500

    Create Statisitics Median in Python.py

---
+++ Statisitics Median in Python.py
@@ -0,0 +1,6 @@
+import statistics     
+datasets = [4, -5, 6, 6, 9, 4, 5, -2]      
+# Printing median of the   
+# random data-set   
+print("Median of data-set is : % s "  
+        % (statistics.median(datasets)))  
